65023baa44a67a84c92e5c911bdee9bba48aaeef
YyepPo <enespaqarizi23@gmail.com>
2025-08-27 19:40:18 +0200

    Projectile functionality added

---
+++ Source/DataDrivenProjectile/Structs/ProjectileData.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NiagaraComponent.h"  // For visuals
+#include "ProjectileData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FProjectileData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		FVector Location;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float Speed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TObjectPtr<UNiagaraComponent> VisualComponent;
+
+	float ZVelocity;
+	FVector Direction;
+};
